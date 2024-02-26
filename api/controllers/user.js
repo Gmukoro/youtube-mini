4e42ac72e91ae5540c659642559b31d696a7e50b
gmukoro <gmukoro3@gmail.com>
2024-02-26 06:07:45 +0100

    Updated my videojs comp

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -56,25 +56,43 @@ export const subscribe = async (req, res, next) => {
 };
 export const unSubscribe = async (req, res, next) => {
   try {
-    await User.findByIdAndUpdate(req.user.id, {
-      $push: { subscribedUsers: req.params.id },
-    });
-    await User.findByIdAndUpdate(req.params.id, {
-      $inc: { subscribers: 1 },
-    });
-    res.status(200).json("Subscription successfull.");
+    try {
+      await User.findByIdAndUpdate(req.user.id, {
+        $pull: { subscribedUsers: req.params.id },
+      });
+      await User.findByIdAndUpdate(req.params.id, {
+        $inc: { subscribers: -1 },
+      });
+      res.status(200).json("Unsubscription successfull.");
+    } catch (err) {
+      next(err);
+    }
   } catch (err) {
     next(err);
   }
 };
 export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+    res.status(200).json("The video has been liked.");
   } catch (err) {
     next(err);
   }
 };
 export const dislike = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id },
+    });
+    res.status(200).json("The video has been disliked.");
   } catch (err) {
     next(err);
   }
