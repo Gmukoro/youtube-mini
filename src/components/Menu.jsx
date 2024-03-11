411c4467df4840b862ca200d581d8a02e46ae5f6
gmukoro <gmukoro3@gmail.com>
2024-03-11 06:36:20 +0100

    Updated icons using material icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,21 @@
+import React from "react";
 import styled from "styled-components";
 import GonaTube from "../img/gonatube-logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import HistoryIcon from "@mui/icons-material/History";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import MovieIcon from "@mui/icons-material/Movie";
+import FeedIcon from "@mui/icons-material/Feed";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ReportIcon from "@mui/icons-material/Report";
+import HelpIcon from "@mui/icons-material/Help";
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
 
 const Container = styled.div`
   flex: 1;
@@ -27,6 +43,7 @@ const Item = styled.div`
   align-items: center;
   gap: 20px;
   cursor: pointer;
+  padding: 7.5px 0px;
 `;
 
 const Menu = () => {
@@ -37,7 +54,67 @@ const Menu = () => {
           <Img src={GonaTube} />
           GonaTube
         </Logo>
-        <Item>Home</Item>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+
+        <Item>
+          <ExploreIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsIcon />
+          Subscriptions
+        </Item>
+        <Item>
+          <VideoLibraryIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryIcon />
+          History
+        </Item>
+        <Item>
+          <LibraryMusicIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsSoccerIcon />
+          Sports
+        </Item>
+        <Item>
+          <Gam />
+          Gaming
+        </Item>
+        <Item>
+          <HomeIcon />
+          Movies
+        </Item>
+        <Item>
+          <HomeIcon />
+          News
+        </Item>
+        <Item>
+          <HomeIcon />
+          Live
+        </Item>
+        <Item>
+          <HomeIcon />
+          Settings
+        </Item>
+        <Item>
+          <HomeIcon />
+          Report
+        </Item>
+        <Item>
+          <HomeIcon />
+          Help
+        </Item>
+        <Item>
+          <HomeIcon />
+          Light Mode
+        </Item>
       </Wrapper>
     </Container>
   );
